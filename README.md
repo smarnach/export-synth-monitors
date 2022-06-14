b4d164a4c2e4845db76482096ca8cde1f7bcbfed
Sven Marnach <sven@mozilla.com>
2022-06-14 14:39:42 +0200

    Query scripts for all scripted monitors.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # New Relic Synthetic Monitor Export
 
-This program queries all synthetic monitors the user has access to from the New Relic NerdGraph API and converts the data to CSV, which is written to stdout.
+This program queries all synthetic monitors the user has access to from the New Relic NerdGraph API and converts the data to CSV, which is written to the file `output/monitor.csv`. For each scripted monitor, the script is written to a file in `output/scripts`.
 
 Environment variables:
 * `NEWRELIC_API_KEY` – a New Relic user key; required
+
+Running:
+```sh
+export NEWRELIC_API_KEY='your_key'
+mkdir -p output/scripts
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,157 @@
-use anyhow::Result;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use serde_json::{from_value, json, Value};
+use std::fs::File;
+
+use anyhow::{Context, Result};
+use serde::de::DeserializeOwned;
+use serde_json::json;
 
 fn main() -> Result<()> {
     let api_key = std::env::var("NEWRELIC_API_KEY")?;
-    let response: Value = query_nerdgraph(QUERY, "", &api_key)?;
-    let entities: Vec<Entity> =
-        from_value(response["data"]["actor"]["entitySearch"]["results"]["entities"].clone())?;
-    let mut wtr = csv::Writer::from_writer(std::io::stdout());
+    let response: monitor::Response = query_nerdgraph(monitor::QUERY, "", &api_key)?;
+    let entities = response.data.actor.entity_search.results.entities;
+    let csv_file = File::create("output/monitor.csv").context("CSV file creation failed")?;
+    let mut wtr = csv::Writer::from_writer(csv_file);
     for entity in entities {
-        wtr.serialize(Monitor::from(entity))?;
+        if entity.monitor_type.starts_with("SCRIPT") {
+            let js = script::get(entity.account_id, &entity.guid, &api_key)?;
+            let name = entity.name.replace('/', "_");
+            std::fs::write(
+                format!("output/scripts/{name}.js"),
+                js,
+            )?;
+        }
+        wtr.serialize(monitor::Monitor::from(entity))?;
     }
     wtr.flush()?;
     Ok(())
 }
 
-const QUERY: &str = r#"{
-  actor {
-    entitySearch(query: "domain = 'SYNTH' AND type = 'MONITOR'") {
-      results {
-        entities {
-          ... on SyntheticMonitorEntityOutline {
-            name
-            accountId
-            monitorType
-            monitoredUrl
-            period
-            tags {
-              key
-              values
+mod monitor {
+    use serde::{Deserialize, Serialize};
+
+    pub const QUERY: &str = r#"{
+      actor {
+        entitySearch(query: "domain = 'SYNTH' AND type = 'MONITOR'") {
+          results {
+            entities {
+              ... on SyntheticMonitorEntityOutline {
+                name
+                accountId
+                guid
+                monitorType
+                monitoredUrl
+                period
+                tags {
+                  key
+                  values
+                }
+              }
             }
           }
         }
       }
+    }"#;
+
+    #[derive(Debug, Deserialize)]
+    pub struct Response {
+        pub data: Data,
     }
-  }
-}"#;
+    #[derive(Debug, Deserialize)]
+    pub struct Data {
+        pub actor: Actor,
+    }
+    #[derive(Debug, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Actor {
+        pub entity_search: Results,
+    }
+    #[derive(Debug, Deserialize)]
+    pub struct Results {
+        pub results: Entities,
+    }
+    #[derive(Debug, Deserialize)]
+    pub struct Entities {
+        pub entities: Vec<Entity>,
+    }
+    #[derive(Debug, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Entity {
+        pub account_id: u32,
+        pub monitor_type: String,
+        pub monitored_url: Option<String>,
+        pub name: String,
+        pub period: u32,
+        pub tags: Vec<Tag>,
+        pub guid: String,
+    }
+    #[derive(Debug, Deserialize)]
+    pub struct Tag {
+        key: String,
+        values: Vec<String>,
+    }
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Entity {
-    account_id: u32,
-    monitor_type: String,
-    monitored_url: Option<String>,
-    name: String,
-    period: u32,
-    tags: Vec<Tag>,
-}
+    #[derive(Debug, Serialize)]
+    pub struct Monitor {
+        account: Option<String>,
+        account_id: u32,
+        name: String,
+        monitor_type: String,
+        monitored_url: Option<String>,
+        period: u32,
+        monitor_status: Option<String>,
+        guid: String,
+    }
 
-#[derive(Debug, Deserialize)]
-struct Tag {
-    key: String,
-    values: Vec<String>,
-}
-
-#[derive(Debug, Serialize)]
-struct Monitor {
-    account: Option<String>,
-    account_id: u32,
-    name: String,
-    monitor_type: String,
-    monitored_url: Option<String>,
-    period: u32,
-    monitor_status: Option<String>,
-}
-
-impl From<Entity> for Monitor {
-    fn from(entity: Entity) -> Self {
-        let mut monitor_status = None;
-        let mut account = None;
-        for tag in entity.tags {
-            if tag.key == "monitorStatus" {
-                monitor_status = tag.values.into_iter().next();
-            } else if tag.key == "account" {
-                account = tag.values.into_iter().next();
+    impl From<Entity> for Monitor {
+        fn from(entity: Entity) -> Self {
+            let mut monitor_status = None;
+            let mut account = None;
+            for tag in entity.tags {
+                if tag.key == "monitorStatus" {
+                    monitor_status = tag.values.into_iter().next();
+                } else if tag.key == "account" {
+                    account = tag.values.into_iter().next();
+                }
+            }
+            Monitor {
+                account_id: entity.account_id,
+                monitor_type: entity.monitor_type,
+                monitored_url: entity.monitored_url,
+                name: entity.name,
+                period: entity.period,
+                monitor_status,
+                account,
+                guid: entity.guid,
             }
         }
-        Monitor {
-            account_id: entity.account_id,
-            monitor_type: entity.monitor_type,
-            monitored_url: entity.monitored_url,
-            name: entity.name,
-            period: entity.period,
-            monitor_status,
-            account,
+    }
+}
+
+mod script {
+    use anyhow::{Error, Result};
+    use serde_json::Value;
+
+    pub fn get(account_id: u32, guid: &str, api_key: &str) -> Result<String> {
+        let query = format!(r#"{{
+          actor {{
+            account(id: {account_id}) {{
+              synthetics {{
+                script(monitorGuid: "{guid}") {{
+                  text
+                }}
+              }}
+            }}
+          }}
+        }}"#);
+        let mut response: Value = super::query_nerdgraph(&query, "", api_key)?;
+        loop {
+            response = match response {
+                Value::Object(map) => map.into_iter().next().unwrap().1,
+                _ => break,
+            }
+        }
+        match response {
+            Value::String(s) => Ok(s),
+            _ => Err(Error::msg("invalid script query response")),
         }
     }
 }
